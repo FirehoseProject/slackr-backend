27534c168da99403b51d9a6543d072cd8a38e197
Ken Mazaika <ken@thefirehoseproject.com>
2016-04-06 12:53:53 -0400

    try again.

--- app/models/chat_message.rb
+++ app/models/chat_message.rb
@@ -3,7 +3,6 @@ class ChatMessage < ActiveRecord::Base
   after_create :push_to_firebase
   belongs_to :api_user
 
-  validate :ensure_user_is_in_same_mode
   def as_json(options={})
     super(options).except("mode").merge("nickname" => self.user.try(:nickname), "avatar_url" => self.user.try(:gravatar_url))
   end
@@ -17,11 +16,6 @@ class ChatMessage < ActiveRecord::Base
   end
 
   private
-  def ensure_user_is_in_same_mode
-    if self.user && self.user.mode != self.mode || self.user.api_user_id != self.api_user_id
-      self.errors.add(:public_key, "invalid permissions")
-    end
-  end
   def push_to_firebase
      FIREBASE.push("#{api_user_key}/messages", self.as_json)
   end
