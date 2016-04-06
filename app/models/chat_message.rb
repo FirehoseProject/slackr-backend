2052d935a62286117c7e7fc72362057907a57660
Ken Mazaika <ken@thefirehoseproject.com>
2016-04-06 12:47:25 -0400

    ensure users can only create messages for the correct accounts.

--- app/models/chat_message.rb
+++ app/models/chat_message.rb
@@ -2,6 +2,8 @@ class ChatMessage < ActiveRecord::Base
   belongs_to :user
   after_create :push_to_firebase
   belongs_to :api_user
+
+  validate :ensure_user_is_in_same_mode
   def as_json(options={})
     super(options).except("mode").merge("nickname" => self.user.try(:nickname), "avatar_url" => self.user.try(:gravatar_url))
   end
@@ -14,6 +16,12 @@ class ChatMessage < ActiveRecord::Base
     end
   end
 
+  private
+  def ensure_user_is_in_same_mode
+    if self.user && self.user.mode != self.mode || self.user.api_user_id != self.api_user_id
+      self.errors.add(:public_key, "invalid permissions")
+    end
+  end
   def push_to_firebase
      FIREBASE.push("#{api_user_key}/messages", self.as_json)
   end
